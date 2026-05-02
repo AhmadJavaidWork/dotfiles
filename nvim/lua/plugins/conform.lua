cdb3948d7135becad738006fc0fdac5e88f774e1
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2026-05-02 10:34:32 +0500

    format for c

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -28,4 +28,14 @@ return {
 			})
 		end, { desc = "Format file or range" })
 	end,
+	opts = {
+		formatters_by_ft = {
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
